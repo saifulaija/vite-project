ea95644566af9b5ebcba86015b5cf5af85521e2d
saifulaija <saifulaija2023@gmail.com>
2024-12-01 17:43:08 +0600

    initial setup done

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import logo from '../public/Logotype.png'
+const App = () => {
+  return (
+    <div className="bg-green-500">
+      <div className="container mx-auto">
+        <div>
+          <img src={logo} alt="" />
+          <p>logo</p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
