128b467a2301f5c89a48322d12f9cff1aa4972b9
saifulaija <saifulaija2023@gmail.com>
2024-12-01 19:43:19 +0600

    header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
+import { X } from "lucide-react";
+import logo from "../public/Logotype.png";
 
-import logo from '../public/Logotype.png'
 const App = () => {
   return (
-    <div className="bg-green-500">
-      <div className="container mx-auto">
-        <div>
-          <img src={logo} alt="" />
-          <p>logo</p>
+    <div className="p-20">
+      <div
+        className="container mx-auto border p-6 relative overflow-hidden"
+     
+      >
+        {/* Content */}
+        <div className="flex flex-col ">
+          <img src={logo} alt="Logo" className="w-60 mb-4" />
+          <div className="flex justify-between">
+            <div className="flex gap-3">
+              <p className="text-lg font-semibold text-gray-700">Home</p>
+              <p className="text-lg font-semibold text-gray-700">Contact</p>
+            </div>
+            <div>
+              <X className="w-6 h-6" />
+            </div>
+          </div>
         </div>
-        <div></div>
       </div>
     </div>
   );
 };
 
 export default App;
+
+
+
